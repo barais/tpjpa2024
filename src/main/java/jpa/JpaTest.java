286cbd46a04a46806dd23984cd878c351def7081
Sophie Gadrey <gadreysophie@gmail.com>
2021-09-09 11:28:02 +0200

    modify jpatest

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,6 +1,9 @@
 package jpa;
 
+import dao.DepartementDao;
 import dao.ProfessionnelDao;
+import dao.RdvDao;
+import dao.UtilisateurDao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,9 +20,17 @@ public class JpaTest {
 				.createEntityManagerFactory("dev");
 		EntityManager manager = factory.createEntityManager();
 
+		DepartementDao departementDao = new DepartementDao(manager);
+		departementDao.createDepartements();
 
-		ProfessionnelDao profdao = new ProfessionnelDao(manager);
-		profdao.createProfessionnels();
+		ProfessionnelDao professionnelDao = new ProfessionnelDao(manager);
+		professionnelDao.createProfessionnels();
+
+		RdvDao rdvDao = new RdvDao(manager);
+		rdvDao.createRdvs();
+
+		UtilisateurDao utilisateurDao = new UtilisateurDao(manager);
+		utilisateurDao.createUtilisateurs();
 
 		manager.close();
 		factory.close();
