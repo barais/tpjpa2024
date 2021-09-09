ab7f95a2765d85aa5a7618b5f9db849370806ead
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-09 08:29:29 +0200

    Modif 1 Jpa Test

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,8 +1,9 @@
 package jpa;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
+import javax.persistence.Persistence;
 
 public class JpaTest {
 
@@ -10,24 +11,23 @@ public class JpaTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("dev");
+		EntityManager manager = factory.createEntityManager();
 
-		EntityManager manager = EntityManagerHelper.getEntityManager();
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
-
-
 		try {
+
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
 
-
 		manager.close();
-		EntityManagerHelper.closeEntityManagerFactory();
-		//		factory.close();
+		factory.close();
 	}
-
 
 }
