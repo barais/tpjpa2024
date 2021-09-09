716733dc4aea671b996319c01b04be7b8aa33241
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-09 11:20:58 +0200

    Modifications syntaxe classes DepartementDao, UtilisateurDao, ProfessionnelDao et RdvDao.

--- src/main/java/dao/DepartementDao.java
+++ src/main/java/dao/DepartementDao.java
@@ -3,9 +3,6 @@ package dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
 import domain.Departement;
 
