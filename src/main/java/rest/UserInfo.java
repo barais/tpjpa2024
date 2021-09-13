03a535a3af0b553fe7fbd5bddd80ada997fc3429
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-13 14:13:18 +0200

    UserInfo restructuré

--- src/main/java/rest/UserInfo.java
+++ src/main/java/rest/UserInfo.java
@@ -10,51 +10,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="userinfo",
+@WebServlet(name="adduserinfo",
         urlPatterns={"/UserInfo"})
 public class UserInfo extends HttpServlet {
 
-    private EntityManagerFactory emf;
+    private EntityManagerFactory factory;
 
     @Override
     public void init() throws ServletException {
-        emf = Persistence.createEntityManagerFactory("dev");
-
-
+        factory = Persistence.createEntityManagerFactory("dev");
     }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    @Override
-    public void doPost(HttpServletRequest request,
-                       HttpServletResponse response)
-            throws ServletException, IOException {
+        // Création de l'objet utilisateur
+        Utilisateur user = new Utilisateur();
+        user.setNom(request.getParameter("nom"));
+
+        // Ajout données à la database
+        EntityManager manager = factory.createEntityManager();
+        manager.getTransaction().begin();
+        manager.persist(user);
+        manager.getTransaction().commit();
+        manager.close();
+
         response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
-
-        Utilisateur utilisateur = new Utilisateur();
-
-        utilisateur.setNom(request.getParameter("nom"));
-        // autres attributs
-
-        EntityManager em = emf.createEntityManager();
-
-
-        em.getTransaction().begin();
-        em.persist(utilisateur);
-        em.getTransaction().commit();
-
-
         out.println("<HTML>\n<BODY>\n" +
-                "<H1>Recapitulatif des informations</H1>\n" +
+                "<H1>Recapitulatif des informations de l'utilisateur</H1>\n" +
                 "<UL>\n" +
-                " <LI>Nom: "
+                " <LI>Nom : "
                 + request.getParameter("nom") +
                 "</UL>\n" +
                 "</BODY></HTML>");
     }
 
-
+    @Override
+    public void destroy() {
+        factory.close();
+    }
 }
