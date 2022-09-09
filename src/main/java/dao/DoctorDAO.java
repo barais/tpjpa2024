05b268cd764c0218f4b518dceb7b9ea2031364dc
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-09 16:39:20 +0200

    refacto DoctorDAO class

--- src/main/java/dao/DoctorDAO.java
+++ src/main/java/dao/DoctorDAO.java
@@ -2,8 +2,10 @@ package dao;
 
 import entities.Doctor;
 
-public class DoctorDAO extends AbstractJpaDao<Long, Doctor>{
-    public DoctorDAO(Class<Doctor> clazzToSet) {
+public class DoctorDAO extends AbstractJpaDao<Long, Doctor> {
+
+    public DoctorDAO() {
         super(Doctor.class);
     }
+
 }
