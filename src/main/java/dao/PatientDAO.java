6362b5f765bd6bb8d205ea1efc9e404a78282e51
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-13 08:50:58 +0200

    change class import

--- src/main/java/dao/PatientDAO.java
+++ src/main/java/dao/PatientDAO.java
@@ -2,7 +2,7 @@ package dao;
 
 import entities.Patient;
 
-public class PatientDAO extends AbstractJpaDao<Long, entities.Patient> {
+public class PatientDAO extends AbstractJpaDao<Long, Patient> {
 
     public PatientDAO() { super(Patient.class); }
 }
