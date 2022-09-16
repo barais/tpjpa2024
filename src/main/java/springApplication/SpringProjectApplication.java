21431b652c23e1aa3b179d630f55873f87350401
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-16 15:02:56 +0200

    changes

--- src/main/java/springApplication/SpringProjectApplication.java
+++ src/main/java/springApplication/SpringProjectApplication.java
@@ -1,4 +1,4 @@
-package com.springproject.springproject;
+package springApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
