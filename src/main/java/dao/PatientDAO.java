7537a0aadf0142f48bbb0ec847fbdf0b2039316c
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-16 14:55:17 +0200

    add spring package

--- src/main/java/dao/PatientDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import entities.Patient;
-
-public class PatientDAO extends AbstractJpaDao<Long, Patient> {
-
-    public PatientDAO() { super(Patient.class); }
-}
--- src/main/java/dao/SpecialisationDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import entities.Specialisation;
-
-public class SpecialisationDAO extends AbstractJpaDao<Long, entities.Specialisation> {
-
-    public SpecialisationDAO() { super(Specialisation.class); }
-}
