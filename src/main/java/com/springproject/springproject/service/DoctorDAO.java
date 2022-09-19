e3fb0d1508e5ee125c442828bb82366dc7d73849
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-19 18:50:47 +0200

    add new query/method in doctor DAO

--- src/main/java/com/springproject/springproject/service/DoctorDAO.java
+++ src/main/java/com/springproject/springproject/service/DoctorDAO.java
@@ -1,11 +1,17 @@
 package com.springproject.springproject.service;
 
 import com.springproject.springproject.domain.Doctor;
+import com.springproject.springproject.domain.Specialisation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface DoctorDAO extends JpaRepository<Doctor, Long> {
 
+    @Query("select d from Doctor as d where d.specialisation = ?1")
+    List<Doctor> getAllBySpecialities(Specialisation spe);
 
 }
