7fa416d6dae3c1e8d198d93ef5c98a6696272bd0
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-19 18:51:01 +0200

    refacto object to enum

--- src/main/java/com/springproject/springproject/domain/Specialisation.java
+++ src/main/java/com/springproject/springproject/domain/Specialisation.java
@@ -1,38 +1,10 @@
 package com.springproject.springproject.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.io.Serializable;
+public enum Specialisation {
 
-@Entity
-public class Specialisation implements Serializable {
+    DENTIST,
+    NEUROLOGIST,
+    GENERALIST,
+    CARDIOLOGIST
 
-    private String name;
-    private Long id;
-
-    public Specialisation() {}
-
-    public Specialisation(String name) {
-        this.name = name;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
 }
