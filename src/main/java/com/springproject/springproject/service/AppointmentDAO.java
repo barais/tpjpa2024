000a17bd585a2f6dd655f03ebfd11e4c98d59508
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-19 14:51:10 +0200

    fix issue JPA

--- src/main/java/com/springproject/springproject/service/AppointmentDAO.java
+++ src/main/java/com/springproject/springproject/service/AppointmentDAO.java
@@ -1,9 +1,10 @@
 package com.springproject.springproject.service;
 
+import com.springproject.springproject.domain.Appointment;
 import com.springproject.springproject.domain.Specialisation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppointmentDAO extends JpaRepository<Specialisation, Long> {
+public interface AppointmentDAO extends JpaRepository<Appointment, Long> {
 }
