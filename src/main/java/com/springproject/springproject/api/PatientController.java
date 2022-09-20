7ee824efe44255ca2aca0dd1fd85c54de8709fda
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-20 15:07:21 +0200

    add DTO for Patient

--- src/main/java/com/springproject/springproject/api/PatientController.java
+++ src/main/java/com/springproject/springproject/api/PatientController.java
@@ -1,11 +1,15 @@
 package com.springproject.springproject.api;
 
+import org.modelmapper.ModelMapper;
 import com.springproject.springproject.domain.Patient;
+import com.springproject.springproject.domain.PatientDTO;
 import com.springproject.springproject.exception.PatientNotFoundException;
 import com.springproject.springproject.service.PatientDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/patients")
@@ -13,29 +17,44 @@ class PatientController {
 
     private final PatientDAO patientDAO;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     PatientController(PatientDAO patientDAO) {
         this.patientDAO = patientDAO;
     }
 
     @GetMapping("")
-    List<Patient> all() {
-        return patientDAO.findAll();
+    List<PatientDTO> all() {
+        //Get all patients in a list
+        List<Patient> listPatients = patientDAO.findAll();
+
+        //convert this list of Patient (entity) to a list of PatientDTO
+        return listPatients
+                .stream()
+                .map(patient -> modelMapper.map(patient, PatientDTO.class))
+                .collect(Collectors.toList());
     }
 
     @PostMapping("")
-    Patient newPatient(@RequestBody Patient newPatient) {
-        return patientDAO.save(newPatient);
+    PatientDTO newPatient(@RequestBody Patient newPatientDTO) {
+        Patient patientEntity = modelMapper.map(newPatientDTO, Patient.class);
+
+        Patient savedPatient = patientDAO.save(patientEntity);
+
+        return modelMapper.map(savedPatient, PatientDTO.class);
     }
 
-    @GetMapping("{id}")
-    Patient one(@PathVariable Long id) throws PatientNotFoundException {
-        return patientDAO.findById(id).orElseThrow(() -> new PatientNotFoundException(id));
+    @GetMapping("/{id}")
+    PatientDTO one(@PathVariable Long id) throws PatientNotFoundException {
+        Patient patient = patientDAO.findById(id).orElseThrow(() -> new PatientNotFoundException(id));
+
+        return modelMapper.map(patient, PatientDTO.class);
     }
 
-    @PutMapping("{id}")
-    Patient replacePatient(@RequestBody Patient newPatient, @PathVariable Long id) {
-
-        return patientDAO.findById(id)
+    @PutMapping("/{id}")
+    PatientDTO replacePatient(@RequestBody Patient newPatient, @PathVariable Long id) {
+        Patient updatedPatient = patientDAO.findById(id)
                 .map(Patient -> {
                     Patient.setFirstName(newPatient.getFirstName());
                     Patient.setLastName(newPatient.getLastName());
@@ -46,9 +65,11 @@ class PatientController {
                     newPatient.setId(id);
                     return patientDAO.save(newPatient);
                 });
+
+        return modelMapper.map(updatedPatient, PatientDTO.class);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     void deletePatient(@PathVariable Long id) {
         patientDAO.deleteById(id);
     }
