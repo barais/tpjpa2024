b9ab1c39ed033b1a0f699d68f58a271cb20f92e7
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-26 11:20:27 +0200

    add a new technical method

--- src/main/java/com/springproject/springproject/service/TimeSlotDAO.java
+++ src/main/java/com/springproject/springproject/service/TimeSlotDAO.java
@@ -1,9 +1,18 @@
 package com.springproject.springproject.service;
 
+import com.springproject.springproject.domain.Doctor;
 import com.springproject.springproject.domain.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Repository
 public interface TimeSlotDAO extends JpaRepository<TimeSlot, Long> {
+
+    @Query("select t from TimeSlot as t where t.date = ?1 and t.doctor = ?2")
+    Optional<TimeSlot> getTimeSlotByDate(Date date, Doctor doctor);
+
 }
