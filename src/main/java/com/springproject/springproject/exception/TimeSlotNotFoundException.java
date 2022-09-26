48cbdcc588ffdf771c66e7bbce9f57229e66286c
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2022-09-26 11:20:53 +0200

    new exception

--- src/main/java/com/springproject/springproject/exception/TimeSlotNotFoundException.java
+++ src/main/java/com/springproject/springproject/exception/TimeSlotNotFoundException.java
@@ -4,4 +4,8 @@ public class TimeSlotNotFoundException extends Exception {
     public TimeSlotNotFoundException(Long id) {
         super("Appointment with id " + id + " not found.");
     }
+    public TimeSlotNotFoundException() {
+        super("Appointment not found.");
+    }
+
 }
