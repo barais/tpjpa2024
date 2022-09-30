ca3eb3d5acb55f1b987572c4e3b4612208d4a38d
troueau <tomrousseau@hotmail.fr>
2022-09-30 16:45:58 +0200

    fix issues with controller

--- src/main/java/com/springproject/springproject/api/SpecialisationController.java
+++ src/main/java/com/springproject/springproject/api/SpecialisationController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/specialisation")
@@ -15,11 +16,17 @@ public class SpecialisationController {
 
     @GetMapping("")
     public List<String> getAllSpecialisation() {
-        return Arrays.stream(Specialisation.values()).map(Specialisation::toString).toList();
+        return Stream.of(Specialisation.values())
+                .map(Specialisation::toString)
+                .collect(Collectors.toList());
     }
+
 
     @GetMapping("/{id}")
     public String getOneSpecialisation(@PathVariable Long id) {
-        return Arrays.stream(Specialisation.values()).toList().get(Math.toIntExact(id)).toString();
+        return Arrays.stream(Specialisation.values())
+                .collect(Collectors.toList())
+                .get(Math.toIntExact(id))
+                .toString();
     }
 }
