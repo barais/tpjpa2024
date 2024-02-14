919dd85b2f98e82900d746063a140e4593a93f8b
nsavoedo <nounagnon-moise-c.savoedo@etudiant.univ-rennes1.fr>
2024-02-14 18:08:08 +0100

    postgres

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -26,6 +26,7 @@ public class JpaTest {
 
 			// TODO create and persist entity
 			test.createUtilisateurs();
+			test.createProjects();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -40,13 +41,22 @@ public class JpaTest {
 	private void createUtilisateurs() {
 		int numOfUtilisateurs = manager.createQuery("Select a From Utilisateur a", Utilisateur.class).getResultList().size();
 		if (numOfUtilisateurs == 0) {
-
 			manager.persist(new Utilisateur("user1", "Jakab Gipsz", Role.DEVELOPER));
 			manager.persist(new Utilisateur("user2","Captain Nemo", Role.MAINTENER));
-
+			manager.persist(new Utilisateur("user3", "Jakab Owner", Role.OWNER));
+			manager.persist(new Utilisateur("user4","Captain Reporter", Role.REPORTER));
+			manager.persist(new Utilisateur("user5", "Jakab DEV2", Role.DEVELOPER));
+			manager.persist(new Utilisateur("user6","Captain DEV2", Role.DEVELOPER));
 		}
 	}
 
+	private void createProjects() {
+		int numOfProjects = manager.createQuery("Select a From Projects a", Projects.class).getResultList().size();
+		if (numOfProjects == 0) {
+			manager.persist(new Projects("SIR Backend"));
+			manager.persist(new Projects("SIR Frontend"));
+		}
+	}
 
 
 }
