4aec5c49e046d1f14843768c2c0188cddbc63dd8
Barais <olivier.barais@gmail.com>
2024-09-11 16:15:39 +0200

    Update show-hsqldb.bat to automatically connect to the database

--- show-hsqldb.bat
+++ show-hsqldb.bat
@@ -1,2 +1,2 @@
-java -cp hsqldb-2.7.2.jar org.hsqldb.util.DatabaseManagerSwing
+java -cp hsqldb-2.7.2.jar org.hsqldb.util.DatabaseManagerSwing --driver org.hsqldb.jdbcDriver --url jdbc:hsqldb:hsql://localhost/ --user SA
 
