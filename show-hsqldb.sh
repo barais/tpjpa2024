bd228fcf4cc9489565fac75dc9c776331316d784
Barais <olivier.barais@gmail.com>
2024-09-11 16:14:42 +0200

    Update show-hsqldb.sh to automatically connect to hsqdbserver

--- show-hsqldb.sh
+++ show-hsqldb.sh
@@ -1,2 +1,2 @@
 #mvn dependency:copy-dependencies
-java -cp hsqldb-2.7.2.jar org.hsqldb.util.DatabaseManagerSwing
+java -cp hsqldb-2.7.2.jar org.hsqldb.util.DatabaseManagerSwing --driver org.hsqldb.jdbcDriver --url jdbc:hsqldb:hsql://localhost/ --user SA
